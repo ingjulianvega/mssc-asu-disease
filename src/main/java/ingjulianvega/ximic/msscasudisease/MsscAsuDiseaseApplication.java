b2edb5c05ea688198c7297277197f840cecf6038
Julian Vega <ingjulianvega@gmail.com>
2021-04-16 08:35:40 -0500

    :zap:Creation. Closes #1 gitignore. Closes #2 README.md. Closes #3

---
+++ src/main/java/ingjulianvega/ximic/msscasudisease/MsscAsuDiseaseApplication.java
@@ -0,0 +1,13 @@
+package ingjulianvega.ximic.msscasudisease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscAsuDiseaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscAsuDiseaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ingjulianvega/ximic/msscasudisease/MsscAsuDiseaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ingjulianvega.ximic.msscasudisease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscAsuDiseaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
