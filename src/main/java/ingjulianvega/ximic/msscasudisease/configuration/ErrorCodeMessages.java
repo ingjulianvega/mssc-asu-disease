e3562df44de454a970fb97f3b8ed77dcba718e4e
Julian Vega <ingjulianvega@gmail.com>
2021-07-18 12:53:14 -0500

    :sparkles: Error messages

--- src/main/java/ingjulianvega/ximic/msscasudisease/configuration/ErrorCodeMessages.java
+++ src/main/java/ingjulianvega/ximic/msscasudisease/configuration/ErrorCodeMessages.java
@@ -6,7 +6,7 @@ import lombok.experimental.UtilityClass;
 public class ErrorCodeMessages {
 
     public static final String DISEASE_NOT_FOUND_API_CODE = "401";
-    public static final String DISEASE_NOT_FOUND_ERROR = "BODY_PART_NOT_FOUND";
+    public static final String DISEASE_NOT_FOUND_ERROR = "DISEASE_NOT_FOUND";
     public static final String DISEASE_NOT_FOUND_MESSAGE = "No se encontró la enfermedad";
     public static final String DISEASE_NOT_FOUND_SOLUTION = "Verifique los datos";
 
